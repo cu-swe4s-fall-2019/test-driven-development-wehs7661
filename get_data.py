81f2df61954c54762564b31004f0a97782ddd8b9
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 01:45:50 -0600

    Modified get_data.py according to the test (test_get_data_invalid_column) added to test_get_data.py

--- get_data.py
+++ get_data.py
@@ -1,3 +1,16 @@
+import sys
+
 
 def read_stdin_col(col_num):
-    return None
+    dataset = []
+    for l in sys.stdin:
+        col = l.splitlines()[0].split()
+        try:
+            data = col[col_num - 1]
+        except IndexError:
+            raise IndexError('Invalid column number')
+        dataset.append(data)
+    return dataset
+
+
+
