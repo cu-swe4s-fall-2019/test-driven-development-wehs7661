686db896cf11ee71351d2fcad0352b2b327c91a0
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-25 22:18:31 -0600

    Modified the function list_stdev in math_lib.py according to the test added in test_math_lib.py, which is test_list_stdev_exceptions.

--- math_lib.py
+++ math_lib.py
@@ -17,6 +17,9 @@ def list_stdev(L):
         return None
     if len(L) == 0:
         return None
-    m = sum(L)/len(L)
+    try:
+        m = sum(L)/len(L)
+    except TypeError:
+        raise TypeError('Unsupported value in the list')  
     std = math.sqrt(sum([(m - x) ** 2 for x in L]) / (len(L) - 1))
     return std
