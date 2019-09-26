218af6901d0f31c728408658198db5f6d6c30916
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 10:24:14 -0600

    Modified get_data.py according to the test added (test_get_data_wrong_type in test_get_data.py

--- get_data.py
+++ get_data.py
@@ -2,15 +2,14 @@ import sys
 
 
 def read_stdin_col(col_num):
-    dataset = []
-    for l in sys.stdin:
-        col = l.splitlines()[0].split()
-        try:
-            data = col[col_num - 1]
-        except IndexError:
-            raise IndexError('Invalid column number')
-        dataset.append(data)
-    return dataset
-
-
-
+   dataset = []
+   for l in sys.stdin:
+       col = l.splitlines()[0].split()
+       try:
+           data = col[col_num - 1]
+       except IndexError:
+           raise IndexError('Invalid column number')
+       except TypeError:
+           raise TypeError('Wrong data type of input')
+       dataset.append(data)
+   return dataset
