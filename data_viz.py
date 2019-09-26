83dc9184d593b63935663b1afaffe97b3d00c0dc
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 14:19:36 -0600

    Modified data_viz.py according to the test added (test_data_viz_hist_exist) to test_data_viz.py

--- data_viz.py
+++ data_viz.py
@@ -4,15 +4,24 @@ import math_lib
 
 
 def boxplot(L, out_file_name):
+    plt.figure()
     plt.boxplot(L)
     plt.xlabel('Box')
     plt.ylabel('Distribution')
-    plt.title('mean: ' + str(math_lib.list_mean(L)) + ' ' +
-              'stdev: ' + str(math_lib.list_stdev(L)))
+    plt.title('Mean: %6.2f, Standard deviation: %6.2f' % (math_lib.list_mean(L), math_lib.list_stdev(L)))
     plt.savefig(out_file_name, dpi = 300)
+    plt.close('all')
     pass
 
+
 def histogram(L, out_file_name):
+    plt.figure()
+    plt.hist(L, bins = 20)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title('Mean: %6.2f, Standard deviation: %6.2f' % (math_lib.list_mean(L), math_lib.list_stdev(L)))
+    plt.savefig(out_file_name, dpi = 300)
+    plt.close('all')
     pass
 
 def combo(L, out_file_name):
