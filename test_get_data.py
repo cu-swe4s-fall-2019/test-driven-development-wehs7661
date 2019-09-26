225a760c8d105a3394670be7e6e63ee362c9ec12
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 10:23:40 -0600

    Added the test method test_get_data_wrong_data

--- test_get_data.py
+++ test_get_data.py
@@ -5,15 +5,14 @@ import sys
 
 
 class TestGetData(unittest.TestCase):
-    def test_get_data_invalid_colnum(self):
-        with self.assertRaises(IndexError) as ex:
-            get_data.read_stdin_col(5)
-        self.assertTrue('Invalid column number' in str(ex.exception))
-        
-
+   def test_get_data_invalid_colnum(self):
+       with self.assertRaises(IndexError) as ex:
+           get_data.read_stdin_col(5)
+       self.assertTrue('Invalid column number' in str(ex.exception))
+   def test_get_data_wrong_type(self):
+       with self.assertRaises(TypeError) as ex:
+           get_data.read_stdin_col('X')
+       self.assertTrue('Wrong data type of input' in str(ex.exception))
+       
 if __name__ == "__main__":
-    unittest.main()
-
-
-
-
+   unittest.main()
