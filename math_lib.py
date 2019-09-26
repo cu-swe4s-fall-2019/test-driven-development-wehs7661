86df0ebde660a67cb018aae9d8e2185a550c890f
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-25 22:32:05 -0600

    Modified math_lib.py to make it conform to the best practices (PEP8 and docstrings)

--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,9 @@
 import math
 
+
 def list_mean(L):
+    """This function returns the mean of a given list
+    """
     if L is None:
         return None
     if len(L) == 0:
@@ -12,7 +15,13 @@ def list_mean(L):
 
     return mean
 
+
 def list_stdev(L):
+    """This function returns the standard deviation of
+    a given list. Note that the standard deviation returned
+    here is the sample standard deviation, which is the
+    same as the formula used in statistics.stdev.
+    """
     if L is None:
         return None
     if len(L) == 0:
@@ -20,6 +29,6 @@ def list_stdev(L):
     try:
         m = sum(L)/len(L)
     except TypeError:
-        raise TypeError('Unsupported value in the list')  
+        raise TypeError('Unsupported value in the list')
     std = math.sqrt(sum([(m - x) ** 2 for x in L]) / (len(L) - 1))
     return std
