ce2041ec0bc1b46c0cadcfde8cf8ebcb0109ce0d
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-25 22:10:21 -0600

    Modified the function list_stdev in math_lib.py according to the test added in test_math_lib.py, which is test_list_stdev_const.

--- math_lib.py
+++ math_lib.py
@@ -1,3 +1,4 @@
+import math
 
 def list_mean(L):
     if L is None:
@@ -16,4 +17,6 @@ def list_stdev(L):
         return None
     if len(L) == 0:
         return None
-    
+    m = sum(L)/len(L)
+    std = math.sqrt(sum([(m - x) ** 2 for x in L]) / len(L))
+    return std
