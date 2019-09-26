26036c2a38e72b25a7537c2ec93f9f3f12d0f30d
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-25 22:26:40 -0600

    Modified test_math_lib.py to make it conform to the best practices (PEP8 and docstrings)

--- test_math_lib.py
+++ test_math_lib.py
@@ -3,16 +3,25 @@ import math_lib
 import random
 import statistics
 
+
 class TestMathLib(unittest.TestCase):
     def test_list_mean_empty(self):
+        """This function tests if the input list is empty
+        """
         r = math_lib.list_mean(None)
         self.assertEqual(r, None)
 
     def test_list_mean_const(self):
-        r = math_lib.list_mean([1,1,1,1])
+        """This function tests if list_mean gives correct
+        result given a list of constants.
+        """
+        r = math_lib.list_mean([1, 1, 1, 1])
         self.assertEqual(r, 1)
 
     def test_list_mean_rand(self):
+        """This function tests if list_mean gives correct
+        result given a list of random numbers.
+        """
         L = []
         for j in range(100):
             for i in range(10):
@@ -22,19 +31,29 @@ class TestMathLib(unittest.TestCase):
             self.assertAlmostEqual(r, e)
 
     def test_list_mean_exceptions(self):
+        """This function tests if an exception is handled properly
+        """
         with self.assertRaises(TypeError) as ex:
             math_lib.list_mean([1, 1, 1, 'x'])
         self.assertTrue('Unsupported value in the list' in str(ex.exception))
 
     def test_list_stdev_empty(self):
+        """This function tests if the input list is empty
+        """
         r = math_lib.list_stdev(None)
         self.assertEqual(r, None)
 
     def test_list_stdev_const(self):
-        r = math_lib.list_stdev([1,1,1,1])
+        """This function tests if list_stdev gives correct
+        result given a list of constants.
+        """
+        r = math_lib.list_stdev([1, 1, 1, 1])
         self.assertEqual(r, 0)
 
     def test_list_stdev_rand(self):
+        """This function tests if list_stdev gives correct
+        result given a list of random numbers.
+        """
         L = []
         for j in range(100):
             for i in range(10):
@@ -42,12 +61,13 @@ class TestMathLib(unittest.TestCase):
             r = math_lib.list_stdev(L)
             e = statistics.stdev(L)
             self.assertAlmostEqual(r, e)
-            
+
     def test_list_stdev_exceptions(self):
+        """This function tests if an exception is handled properly
+        """
         with self.assertRaises(TypeError) as ex:
             math_lib.list_stdev([1, 1, 1, 'x'])
         self.assertTrue('Unsupported value in the list' in str(ex.exception))
-    
 
 
 if __name__ == "__main__":
