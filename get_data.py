1f0c1370262e6ad5a1a494c73282fb925a0a2855
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 10:48:30 -0600

    Modified get_data.py to make it conform to best practices (PEP8 and docstring)

--- get_data.py
+++ get_data.py
@@ -2,14 +2,29 @@ import sys
 
 
 def read_stdin_col(col_num):
-   dataset = []
-   for l in sys.stdin:
-       col = l.splitlines()[0].split()
-       try:
-           data = col[col_num - 1]
-       except IndexError:
-           raise IndexError('Invalid column number')
-       except TypeError:
-           raise TypeError('Wrong data type of input')
-       dataset.append(data)
-   return dataset
+    """This function returns an array corresponding to values from
+    stdin at the column number position
+
+    Parameters
+    ----------
+    col_num : int
+        column number
+
+    Returns
+    -------
+    dataset : list
+        An list of values, which is a part of data from stdin
+
+    """
+
+    dataset = []
+    for l in sys.stdin:
+        col = l.splitlines()[0].split()
+        try:
+            data = col[col_num - 1]
+        except IndexError:
+            raise IndexError('Invalid column number')
+        except TypeError:
+            raise TypeError('Wrong data type of input')
+        dataset.append(data)
+    return dataset
