6d31e5741289bf928d86c549acf8b303ad92d5fb
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 10:47:42 -0600

    Modified test_get_data.py to make it conform to best practices (PEP8 and docstring)

--- test_get_data.py
+++ test_get_data.py
@@ -5,14 +5,20 @@ import sys
 
 
 class TestGetData(unittest.TestCase):
-   def test_get_data_invalid_colnum(self):
-       with self.assertRaises(IndexError) as ex:
-           get_data.read_stdin_col(5)
-       self.assertTrue('Invalid column number' in str(ex.exception))
-   def test_get_data_wrong_type(self):
-       with self.assertRaises(TypeError) as ex:
-           get_data.read_stdin_col('X')
-       self.assertTrue('Wrong data type of input' in str(ex.exception))
-       
+    def test_get_data_invalid_colnum(self):
+        """This function test if the column number is valid
+        """
+        with self.assertRaises(IndexError) as ex:
+            get_data.read_stdin_col(5)
+        self.assertTrue('Invalid column number' in str(ex.exception))
+
+    def test_get_data_wrong_type(self):
+        """This function test if the input data type is correct
+        """
+        with self.assertRaises(TypeError) as ex:
+            get_data.read_stdin_col('X')
+        self.assertTrue('Wrong data type of input' in str(ex.exception))
+
+
 if __name__ == "__main__":
-   unittest.main()
+    unittest.main()
