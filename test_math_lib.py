43df41023d27e8535de52adfda1db796f22e8289
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-25 21:13:17 -0600

    Added the test called test_list_mean_const

--- test_math_lib.py
+++ test_math_lib.py
@@ -1,9 +1,14 @@
 import unittest
 import math_lib
 
-class TestMathLib(unittest.testCase):
+class TestMathLib(unittest.TestCase):
     def test_list_mean_empty(self):
         r = math_lib.list_mean(None)
         self.assertEqual(r, None)
 
-    
+    def test_list_mean_const(self):
+        r = math_lib.list_mean([1,1,1,1])
+        self.assertEqual(r, 1)
+
+if __name__ == "__main__":
+    unittest.main()
