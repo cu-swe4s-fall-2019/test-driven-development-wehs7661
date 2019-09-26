480bf85c3b8f2808cd1317671f7c43134e2d4bd5
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-26 14:33:36 -0600

    Added test_data_viz_combo_exist to test_data_viz.py

--- test_data_viz.py
+++ test_data_viz.py
@@ -5,8 +5,9 @@ import os
 
 
 class TestDataViz(unittest.TestCase):
+    global L
+    L = get_data.read_stdin_col(1)
     def test_data_viz_boxplot_exist(self):
-        L = get_data.read_stdin_col(1)
         output_name = 'boxplot_test.png'
         check_bf = os.path.exists(output_name)
         data_viz.boxplot(L, output_name)
@@ -15,10 +16,17 @@ class TestDataViz(unittest.TestCase):
         self.assertTrue(check_af)
 
     def test_data_viz_hist_exist(self):
-        L = get_data.read_stdin_col(1)
         output_name = 'hist_test.png'
         check_bf = os.path.exists(output_name)
         data_viz.histogram(L, output_name)
+        check_af = os.path.exists(output_name)
+        self.assertFalse(check_bf)
+        self.assertTrue(check_af)
+
+    def test_data_viz_combo_exist(self):
+        output_name = 'combo_test.png'
+        check_bf = os.path.exists(output_name)
+        data_viz.combo(L, output_name)
         check_af = os.path.exists(output_name)
         self.assertFalse(check_bf)
         self.assertTrue(check_af)
